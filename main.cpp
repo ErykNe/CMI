091b9f01554f52288f57011a50dc321bda354c86
Utopekk <120266083+Utopekk@users.noreply.github.com>
2022-12-19 21:17:31 +0100

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int T = 0; //ile zestawow budynkow jest
 int licznik; //ilość wyburzeń
 
 void alg();
-int znajdzNajwiekszaLiczbe(int low, int high);
+int binarySearch(int low, int high);
 int main() {
     cin >> T;
     for (int i = 0; i < T; ++i) {
@@ -23,23 +23,67 @@ int main() {
             koglomerat.push_back(pietro);
         }
     }
+    for (int pri : koglomerat) {
+        cout << pri << " "; //kontrolnie
+    }
+    cout << T << "\n";
+    cout << "\n";
+
     alg();
+
+
     return 0;
 }
 int sd = 0;
 void alg(){
     for (int i = 0; i < IloscBudynkow.size(); ++i) {
-        //algorytm
-        //jezeli znalazlo 0 w środku tego - Cout << "NIE" << "\n";
-        //continue;
-        int ilosc = znajdzNajwiekszaLiczbe(0, IloscBudynkow[i]); //znajdz najwieksza liczbe
+        int ilosc = binarySearch(0, IloscBudynkow[i]); //znajdz najwieksza liczbe
+        cout << '\n';
         cout << ilosc << "\n";
+
         for (int j = 0; j < IloscBudynkow[i]; ++j) {
             koglomerat.erase(koglomerat.begin());
         }
     }
 }
-int znajdzNajwiekszaLiczbe(int low, int high){
+int binarySearch(int low, int high){
     int maxElement = *max_element(koglomerat.begin(), koglomerat.end());
     return maxElement;
+}
+void BinarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+ 
+        // If the element is present at the middle
+        // itself
+        if (arr[mid] == x)
+            return mid;
+ 
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+ 
+        // Else the element can only be present
+        // in right subarray
+        return binarySearch(arr, mid + 1, r, x);
+    }
+ 
+    // We reach here when element is not
+    // present in array
+    return -1;
+}
+    int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, 0, n - 1, x);
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+    return 0;
+}
+    cout << "NIE" << "\n";
 }
