55f3241f3636a64c465fb23645e5b585665b61ff
ErykNe <110306022+ErykNe@users.noreply.github.com>
2022-12-19 19:42:13 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> koglomerat;
+vector<int> IloscBudynkow;
+int T = 0; //ile zestawow budynkow jest
+int licznik; //ilość wyburzeń
+
+void alg();
+int main() {
+    cin >> T;
+    for (int i = 0; i < T; ++i) {
+        int N = 0; //ile budynkow jest w jednym
+        cin >> N;
+        IloscBudynkow.push_back(N);
+        for (int j = 0; j < N; ++j) {
+            int pietro;
+            cin >> pietro;
+            koglomerat.push_back(pietro);
+        }
+    }
+    for (int pri : koglomerat) {
+        cout << pri << " "; //kontrolnie
+    }
+    cout << T << "\n";
+    cout << "\n";
+
+    alg();
+
+
+    return 0;
+}
+int sd = 0;
+void alg(){
+    for (int i = 0; i < T; ++i) {
+        while (true){
+            for (int j = 0; j < IloscBudynkow[i] - 1; ++j) {
+                if (koglomerat[j] == 0){
+                    sd++; // tu do przekminy
+                } else {
+                    koglomerat[j]--;
+                }
+            }
+            licznik++;
+            if (sd == IloscBudynkow[i]){
+                cout << licznik << "\n";
+                break;
+            }
+            sd = 0;
+        }
+    }
+}
