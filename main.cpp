fdc76c634f362e068144b87200dbc4b7969c2f1b
Utopekk <120266083+Utopekk@users.noreply.github.com>
2022-12-19 20:11:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -51,4 +51,17 @@ void alg(){
             sd = 0;
         }
     }
+    // Get the vector
+    vector<int> a = { 1, 45, 54, 71, 76, 12 };
+  
+    // Print the vector
+    cout << "Vector: ";
+    for (int i = 0; i < a.size(); i++)
+        cout << a[i] << " ";
+    cout << endl;
+  
+    // Find the max element
+    cout << "\nMax Element = "
+         << *max_element(a.begin(), a.end());
+    return 0;
 }
