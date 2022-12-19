262a182208643cca06087f1d873342e5cd10c9ca
Utopekk <120266083+Utopekk@users.noreply.github.com>
2022-12-19 21:21:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -50,18 +50,18 @@ int binarySearch(int low, int high){
     int maxElement = *max_element(koglomerat.begin(), koglomerat.end());
     return maxElement;
 }
-void BinarySearch(int arr[], int l, int r, int x)
+void BinarySearch( int l, int r, int x)
 {
     if (r >= l) {
         int mid = l + (r - l) / 2;
  
-        if (arr[mid] == x)
+        if (IloscBudynkow[mid] == x)
             return mid;
  
-        if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x);
+        if (IloscBudynkow[mid] > x)
+            return binarySearch(IloscBudynkow, l, mid - 1, x);
  
-        return binarySearch(arr, mid + 1, r, x);
+        return binarySearch(IloscBudynkow, mid + 1, r, x);
     }
  
     return -1;
@@ -70,7 +70,7 @@ int main(void)
 {
     int arr[] = { 2, 3, 4, 10, 40 };
     int x = 10;
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n = sizeof(IloscBudynkow) / sizeof(IloscBudynkow[0]);
     int result = binarySearch(arr, 0, n - 1, x);
     (result == -1)
         ? cout << "Element is not present in array"
