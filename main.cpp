977490b8e759180b2095778c9b2bc0505e20d121
ErykNe <110306022+ErykNe@users.noreply.github.com>
2022-12-20 10:57:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@ int T = 0; //ile zestawow budynkow jest
 int licznik; //ilość wyburzeń
 
 void alg();
+
 int znajdzNajwiekszaLiczbe(int low, int high);
-int znajdzZera(int low, int high);
+int binarySearch(int l, int r, int x);
+
 int main() {
     cin >> T;
     for (int i = 0; i < T; ++i) {
@@ -33,11 +35,15 @@ void alg(){
         //algorytm
         //jezeli znalazlo 0 w środku tego - Cout << "NIE" << "\n";
         //continue;
-        int a = znajdzZera(0, IloscBudynkow[i]);
-        if (a == 1){
-            cout << "NIE" << "\n";
-            continue;
+        int a = binarySearch(0, IloscBudynkow[i], 0);
+        int centrum = 0 + (IloscBudynkow[i] - 0) / 2;
+        if (a <= centrum){
+
         }
+        if(a > centrum){
+
+        }
+        cout << a << "\n";
         int ilosc = znajdzNajwiekszaLiczbe(0, IloscBudynkow[i]); //znajdz najwieksza liczbe
         cout << ilosc << "\n";
         for (int j = 0; j < IloscBudynkow[i]; ++j) {
@@ -46,14 +52,26 @@ void alg(){
     }
 }
 int znajdzNajwiekszaLiczbe(int low, int high){
-    int maxElement = *max_element(koglomerat.begin(), koglomerat.end());
+    int maxElement = *max_element(koglomerat.begin(), koglomerat.begin() + high);
     return maxElement;
 }
-int znajdzZera(){
-    try{
-        auto it = find(IloscBudynkow.begin(), IloscBudynkow.end(), 0);
-        znajdzZera(0, it); //recall - naprawic
-    } catch (exception e){
-        return 1;
+int binarySearch(int low, int high, int szukana) {
+    if (high >= low) {
+        int centrum = low + (high - low) / 2;
+
+
+        if (koglomerat[centrum] == szukana){
+            return centrum;
+        }
+`       else if (koglomerat[centrum] > szukana){
+            return binarySearch(low, centrum - 1, 0);
+        }
+        else if (koglomerat[centrum] < szukana){
+            return binarySearch(centrum + 1, high, 0);
+        }
     }
+
+    // We reach here when element is not
+    // present in array
+    return -1;
 }
