febc6eadc4539b8b8fc9c8f14228362c5274bddc
ErykNe <110306022+ErykNe@users.noreply.github.com>
2022-12-21 18:10:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,17 @@ using namespace std;
 
 vector<int> koglomerat;
 vector<int> IloscBudynkow;
-int T = 0; 
-int licznik; 
+int T = 0;
 
 void alg();
 
 int znajdzNajwiekszaLiczbe(int low, int high);
-int binarySearch(int l, int r, int x);
+int sprawdzOptymalnosc(int low, int high);
 
 int main() {
     cin >> T;
     for (int i = 0; i < T; ++i) {
-        int N = 0; 
+        int N = 0;
         cin >> N;
         IloscBudynkow.push_back(N);
         for (int j = 0; j < N; ++j) {
@@ -29,10 +28,9 @@ int main() {
     alg();
     return 0;
 }
-int sd = 0;
 void alg(){
     for (int i = 0; i < IloscBudynkow.size(); ++i) {
-        int a = binarySearch(0, IloscBudynkow[i], 0);
+       int a = sprawdzOptymalnosc(0, IloscBudynkow[i]);
         if (a == -1){
             cout << "NIE" << "\n";
             for (int j = 0; j < IloscBudynkow[i]; ++j) {
@@ -40,7 +38,7 @@ void alg(){
             }
             continue;
         }
-        int ilosc = znajdzNajwiekszaLiczbe(0, IloscBudynkow[i]); 
+        int ilosc = znajdzNajwiekszaLiczbe(0, IloscBudynkow[i]);
         cout << ilosc << "\n";
         for (int j = 0; j < IloscBudynkow[i]; ++j) {
             koglomerat.erase(koglomerat.begin());
@@ -51,19 +49,47 @@ int znajdzNajwiekszaLiczbe(int low, int high){
     int maxElement = *max_element(koglomerat.begin(), koglomerat.begin() + high);
     return maxElement;
 }
-int binarySearch(int low, int high, int szukana) {
-
-    int k = 0;
-    
-    for(int i = high - 1; i >= low; i--){
+int sprawdzOptymalnosc(int low, int high) {
+    for(int i = high - 1; i > low; i--){
         int a = koglomerat[i];
         if(a == 0){
-            if(koglomerat[i + 1] != 0 && koglomerat[i - 1] != 0 && koglomerat[i + 1] < high){
+            if(koglomerat[i + 1] != 0 && koglomerat[i - 1] != 0 && i + 1 < high){
                 return -1;
                 break;
             }
         }
     }
-    
-    return 0;
+    int maxElement = *max_element(koglomerat.begin(), koglomerat.begin() + high);
+    vector<int>::iterator i = max_element(koglomerat.begin(), koglomerat.begin() + high);
+    int indexMaxElement = std::distance(koglomerat.begin(), i);
+    int secondLargest = 0;
+    for (int i = 0; i < high; i++) {
+        if (koglomerat[i] != koglomerat[indexMaxElement]) {
+            if (secondLargest == -1) {
+                secondLargest = i;
+            }
+            else if (koglomerat[i] > koglomerat[secondLargest]) {
+                secondLargest = i;
+            }
+        }
+    }
+    int secondSecondLargest = 0;
+    for (int i = high - 1; i >= 0; i--) {
+        if (koglomerat[i] != koglomerat[indexMaxElement]) {
+            if (secondSecondLargest == -1) {
+                secondSecondLargest = i;
+            }
+            else if (koglomerat[i] > koglomerat[secondSecondLargest]) {
+                secondSecondLargest = i;
+            }
+        }
+    }
+    if (abs(indexMaxElement - secondLargest) > 1){
+        return -1;
+    }
+    else if (abs(indexMaxElement - secondSecondLargest) > 1){
+        return -1;
+    } else {
+        return 0;
+    }
 }
