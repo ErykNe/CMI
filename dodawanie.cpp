c631dbc9458d5554eb186e515d26831973654ae3
Utopekk <120266083+Utopekk@users.noreply.github.com>
2023-01-04 15:08:11 +0100

    Update dodawanie.cpp

--- dodawanie.cpp
+++ dodawanie.cpp
@@ -7,11 +7,11 @@ const char* jednosci[10] = {"", "jeden", "dwa", "trzy", "cztery", "piec", "szesc
 const char* nascie[10] = {"dziesiec", " jedenascie", " dwanascie", " trzynascie", " czternascie", " pietnascie", " szesnascie", " siedemnascie", " osiemnascie", " dziewietnascie"};
 const char* dziesiatki[10] ={"", " dziesiec", " dwadziescia", " trzydziesci", " czterdziesci", " piecdziesiat", " szescdziesiat", " siedemdziesiat", " osiemdziesiat", " dziewiecdziesiat"};
 const char* setki[10] = {"", " sto", " dwiescie", " trzysta", " czterysta", " piecset", " szescset", " siedemset", " osiemset", " dziewiecset"};
-const char* x[7] = {"", " tys.", " mln.", " mld.", " bln.", " bld.", " try."};
+const char* x[7] = {"", " tysiecy", " milionow", " tysiace", " miliony", " tysiac", " milion"};
 
 using namespace std;
 
-vector<pair<string, string>> pary = {}; //para - typ danych ktory przechowuje dwie dane i tylko dwie, utworzylem z nich vector ktory przechowuje pary
+vector<pair<string, string> > pary = {}; //para - typ danych ktory przechowuje dwie dane i tylko dwie, utworzylem z nich vector ktory przechowuje pary
 
 int slowaNaLiczbe(string slowo);
 string liczbaNaSlowa(int liczba);
@@ -22,10 +22,11 @@ int main() {
     cin >> t;
     for (int i = 0; i < t; ++i) {
 
+       
         string liczba;
-        cin >> liczba;
+        getline(std::cin >> std::ws, liczba);
         string liczba2;
-        cin >> liczba2;
+        getline(std::cin >> std::ws, liczba2);
         pair<string, string> para;
         para.first = liczba;
         para.second = liczba2;
@@ -41,7 +42,6 @@ int main() {
 
     return 0;
 }
-
 int slowaNaLiczbe(string slowo){
     int liczba;
 
